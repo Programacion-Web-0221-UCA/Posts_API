2fea1f91801c15c88b1262662086229fdeb1b239
DouglasHdezT <00004516@uca.edu.sv>
2021-11-09 21:15:59 -0600

    :construction: Add: Routes

--- routes/api.router.js
+++ routes/api.router.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const { authRequired } = require("@app/middlewares/auth.middlewares");
 
 const authRouter = require("./api/auth.router");
+const postRouter = require("./api/post.router");
 
 router.use("/auth", authRouter);
 router.use(authRequired);
 
+router.use("/post", postRouter);
 
 module.exports = router;
