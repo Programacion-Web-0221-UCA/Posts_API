3e32af8f8106bd288a2c788f74e2053f7e0ee975
DouglasHdezT <00004516@uca.edu.sv>
2021-11-09 23:28:28 -0600

    :bug: Fix minor errors

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ var logger = require('morgan');
 const database = require("@app/config/mongoose.config");
 const mainRouter = require("@app/routes");
 const { errorHandler } = require("@app/middlewares/handler.middlewares");
+const cors = require("cors");
 
 var app = express();
 
 database.connect();
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
